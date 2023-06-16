36b7adcdccfdaf6a4c1a21dd1da0bbe085d3ef3e
Roberto Alsina <roberto.alsina@gmail.com>
2023-06-16 17:49:47 -0300

    Rudimentary task creation

--- spec/hace_spec.cr
+++ spec/hace_spec.cr
@@ -5,8 +5,14 @@ describe Hace do
 end
 
 describe "parse_file" do
+
   it "should parse the tasks section" do
     parsed = parse_file("spec/testcases/basic/Hacefile.yml")
     parsed["tasks"].as_h.keys.should eq ["foo"]
+  end
+
+  it "should create tasks for all tasks" do
+    create_tasks(parse_file("spec/testcases/basic/Hacefile.yml"))
+    TaskManager.tasks.keys.should eq ["foo"]
   end
 end
--- src/hace.cr
+++ src/hace.cr
@@ -1,4 +1,7 @@
 require "yaml"
+require "croupier"
+
+include Croupier
 
 module Hace
   VERSION = "0.1.0"
@@ -7,9 +10,16 @@ module Hace
     config = File.open(path, "r") do |file|
       YAML.parse(file).as_h
     end
-    config.each do |key, value|
-      p! key
-    end
     config
+  end
+
+  # Create tasks out of config
+  def create_tasks(config)
+    config["tasks"].as_h.each do |name, data|
+      data = data.as_h
+      output = data.fetch("output", name).as_s
+      inputs = data.fetch("dependencies", YAML.parse("[]")).as_a.map(&.as_s)
+      Task.new(name:name.as_s, output:output, inputs: inputs)
+    end
   end
 end
