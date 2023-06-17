07457cf480b3556587d20e293505648ceb7d1c3d
Roberto Alsina <roberto.alsina@gmail.com>
2023-06-16 21:55:58 -0300

    Very basic stuff working

--- spec/hace_spec.cr
+++ spec/hace_spec.cr
@@ -5,7 +5,6 @@ describe Hace do
 end
 
 describe "parse_file" do
-
   it "should parse the tasks section" do
     parsed = parse_file("spec/testcases/basic/Hacefile.yml")
     parsed["tasks"].as_h.keys.should eq ["foo"]
@@ -14,5 +13,19 @@ describe "parse_file" do
   it "should create tasks for all tasks" do
     create_tasks(parse_file("spec/testcases/basic/Hacefile.yml"))
     TaskManager.tasks.keys.should eq ["foo"]
+    TaskManager.tasks["foo"].to_s.should eq \
+      "Commands: \n" + "  echo \"make foo out of bar\" > foo\n" + "  cat bar >> foo"
+  end
+
+  it "should execute the command of a task" do
+    Dir.cd("spec/testcases/basic") do
+      create_tasks(parse_file("Hacefile.yml"))
+      File.delete?("foo")
+      File.open("bar", "w") do |io|
+        io << "quux\n"
+      end
+      TaskManager.run_tasks
+      File.read("foo").should eq "make foo out of bar\nquux\n"
+    end
   end
 end
--- src/hace.cr
+++ src/hace.cr
@@ -6,6 +6,30 @@ include Croupier
 module Hace
   VERSION = "0.1.0"
 
+  class CommandTask < Task
+    @commands : Array(String)
+
+    # @args : Array(String)
+
+    def initialize(name, commands : String, output, inputs, no_save)
+      @commands = commands.split("\n").map(&.strip).reject(&.empty?)
+      @procs = @commands.map do |command|
+        TaskProc.new { Process.run(
+          command: command,
+          shell: true,
+        ).to_s }
+      end
+      super(
+        name: name, output: output,
+        inputs: inputs, no_save: no_save
+      )
+    end
+
+    def to_s
+      "Commands: \n  #{@commands.join("\n  ")}"
+    end
+  end
+
   def parse_file(path = "Hacefile.yml")
     config = File.open(path, "r") do |file|
       YAML.parse(file).as_h
@@ -20,7 +44,14 @@ module Hace
       data = data.as_h
       output = data.fetch("output", name).as_s
       inputs = data.fetch("dependencies", YAML.parse("[]")).as_a.map(&.as_s)
-      Task.new(name:name.as_s, output:output, inputs: inputs)
+      commands = data["commands"].as_s
+      CommandTask.new(
+        name: name.as_s,
+        commands: commands,
+        output: output,
+        inputs: inputs,
+        no_save: true,
+      )
     end
   end
 end
