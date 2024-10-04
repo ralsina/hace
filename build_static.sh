1afeee83016116425013aa7d3e38c8f91e561aa5
Roberto Alsina <roberto.alsina@gmail.com>
2024-10-04 13:57:47 -0300

    fix: make static binaries really static

--- build_static.sh
+++ build_static.sh
@@ -7,10 +7,10 @@ docker run --rm --privileged \
 
 # Build for AMD64
 docker build . -f Dockerfile.static -t hace-builder
-docker run -ti --rm -v "$PWD":/app --user="$UID" hace-builder /bin/sh -c "cd /app && rm -rf lib shard.lock && shards build --release --without-development"
+docker run -ti --rm -v "$PWD":/app --user="$UID" hace-builder /bin/sh -c "cd /app && rm -rf lib shard.lock && shards build --release --without-development --static"
 mv bin/hace bin/hace-static-linux-amd64
 
 # Build for ARM64
 docker build . -f Dockerfile.static --platform linux/arm64 -t hace-builder
-docker run -ti --rm -v "$PWD":/app --platform linux/arm64 --user="$UID" hace-builder /bin/sh -c "cd /app && rm -rf lib shard.lock && shards build --release --without-development"
+docker run -ti --rm -v "$PWD":/app --platform linux/arm64 --user="$UID" hace-builder /bin/sh -c "cd /app && rm -rf lib shard.lock && shards build --release --without-development --static"
 mv bin/hace bin/hace-static-linux-arm64
